dedd7d65ca0210c2678231c9859f292fb9c6414e
Edward Ng <edwardlng@outlook.com>
2023-09-06 20:11:55 -0400

    Added file input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react';
+import FileInput from './components/FileInput';
 import './App.css';
 
 function App() {
+  const [fileUrls, setFileUrls] = useState<string[]>([]);
+  const [inputError, setInputError] = useState<string>('');
+
+  // A list of all accepted file types.
+  const accept : string = '.shp, .shx, .dbf, ' // Shape Files
+    + '.json, .geojson, application/geo+json, ' // GeoJSON Files
+    + '.kml, .kmz, application/vnd.google-earth.kml+xml, ' // Keyhole Files
+      + 'application/vnd.google-earth.kmz';
+
+  const handleFiles = useCallback(
+    (event : React.ChangeEvent<HTMLInputElement>) => {
+      // Cleanup.
+      setInputError('');
+      fileUrls.forEach((file : string) => URL.revokeObjectURL(file));
+
+      // Load each new file as a blob URL.
+      const fileList : FileList | null = event.target.files;
+      if (fileList) {
+        // Validate File Types.
+        // TODO: Handle more file extensions listed in the accept string.
+        // TODO: Verify files more rigorously (not through file extension).
+        // TODO: Verify file combinations.
+        for (let i = 0; i < fileList.length; i += 1) {
+          if (!/.(shp|dbf|json|kml)/.test(fileList[0].name)) {
+            setInputError('File types must be .shp, .dbf, .json, or .kml');
+            return;
+          }
+        }
+
+        const newFileUrls : string[] = [];
+        for (let i = 0; i < fileList.length; i += 1) {
+          newFileUrls.push(URL.createObjectURL(fileList[i]));
+        }
+        setFileUrls(newFileUrls);
+      } else {
+        // TODO: Warn the user that the file list is null.
+      }
+    },
+    [setInputError, fileUrls, setFileUrls],
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FileInput id="map-file-input" accept={accept} onChange={handleFiles}>
+        Choose a Map to Render:
+      </FileInput>
+      { inputError ? <p>{inputError}</p> : '' }
     </div>
   );
 }
